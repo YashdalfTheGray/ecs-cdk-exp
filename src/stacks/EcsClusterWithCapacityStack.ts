71551b5cbf2c851a873966474848909db9ca7dd9
Yash Kulshrestha <kulshres@amazon.com>
2023-02-07 23:16:33 -0800

    added a stack that spins up a cluster with capacity

---
+++ src/stacks/EcsClusterWithCapacityStack.ts
@@ -0,0 +1,36 @@
+import { Stack, App, StackProps } from 'aws-cdk-lib';
+import {
+  InstanceType,
+  InstanceClass,
+  InstanceSize,
+  SubnetType,
+  SecurityGroup,
+  Peer,
+  Port,
+} from 'aws-cdk-lib/aws-ec2';
+
+import { Cluster } from 'aws-cdk-lib/aws-ecs';
+
+export class EcsClusterWithCapacityStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const testCluster = new Cluster(this, 'EcsCluster', {
+      clusterName: 'envvar-example-cluster',
+      capacity: {
+        instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+        autoScalingGroupName: `${id}ClusterAsg`,
+        associatePublicIpAddress: true,
+        keyName: 'ssh-access',
+        vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      },
+    });
+
+    const securityGroup = new SecurityGroup(this, 'EcsSecurityGroup', {
+      vpc: testCluster.vpc,
+    });
+
+    securityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(80));
+    securityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(22));
+  }
+}
