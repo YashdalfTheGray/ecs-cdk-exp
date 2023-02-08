bb2cdf4c86fb822b30019635caa6c5e4ac7356ca
Yash Kulshrestha <kulshres@amazon.com>
2023-02-07 23:17:22 -0800

    added the ecs cluster stack to the index, deleted the cloudwatch only role

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import 'source-map-support/register';
 import * as dotenv from 'dotenv';
 import { App } from 'aws-cdk-lib';
 
+import { EcsClusterWithCapacityStack } from './stacks';
+
 dotenv.config();
 
 const app = new App();
+new EcsClusterWithCapacityStack(app, 'EcsLbExpStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
